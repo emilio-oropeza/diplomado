2264517755d9f03eb4c18ba3fe7a93bdffdc2888
Vocho <vocho@underdog.gs>
2016-04-01 15:47:24 -0600

    cambios
    Shio

--- js/desde.js
+++ js/desde.js
@@ -7,17 +7,71 @@ $(document).ready(function(){
 
 	$("#portada").waypoint(function(direction){
 		if(direction == "down"){
-			$(".navbar-inverse .navbar-nav > li > a").removeClass("height_100");
-			$(".navbar-brand").removeClass("height_100");
-			$("#diplo_navbar").height("60px");
-			$("body").css({"padding-top":"60px"});
+			$(".navbar-inverse .navbar-nav > li > a").animate({
+				"padding-top":"20px",
+				"padding-bottom": "20px"
+			}, 500);
+			$(".navbar-brand").animate({
+				"padding-top":"20px"
+			}, 500);
+			$("#diplo_navbar").animate({
+				"height":"60px"
+			}, 500);
+			$("body").animate({
+				"padding-top": "60px"
+			}, 500);
 		}else{
-			$(".navbar-inverse .navbar-nav > li > a").addClass("height_100");
-			$(".navbar-brand").addClass("height_100");
 			if($(window).width() > 767){
-				$("#diplo_navbar").height("100px");
-				$("body").css({"padding-top":"100px"});
+				$(".navbar-inverse .navbar-nav > li > a").animate({
+					"padding-top":"40px",
+					"padding-bottom": "40px"
+				}, 500);
+				$(".navbar-brand").animate({
+					"padding-top":"40px"
+				}, 500);
+				$("#diplo_navbar").animate({
+					"height":"100px"
+				}, 500);
+				$("body").animate({
+					"padding-top": "100px"
+				}, 500);
 			}
 		}
 	},{offset:'0%'});
+	$(window).resize(function(){
+		if($(window).width() <= 767){
+			$(".navbar-inverse .navbar-nav > li > a").css({
+				"padding-top":"20px",
+				"padding-bottom": "20px"
+			});
+			$(".navbar-brand").css({
+				"padding-top":"20px"
+			});
+			$("#diplo_navbar").css({
+				"height":"60px"
+			});
+			$("body").css({
+				"padding-top": "60px"
+			});
+		}else{
+			$(".navbar-inverse .navbar-nav > li > a").css({
+				"padding-top":"40px",
+				"padding-bottom": "40px"
+			});
+			$(".navbar-brand").css({
+				"padding-top":"40px"
+			});
+			$("#diplo_navbar").css({
+				"height":"100px"
+			});
+			$("body").css({
+				"padding-top": "100px"
+			});
+		}
+	});
+
+
+
+
 });
+
